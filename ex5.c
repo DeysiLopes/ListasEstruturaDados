4ea88cf34ea271525d9f43623b23dbc9fca76a5b
DeysiLopes <deysilinconl@gmail.com>
2022-05-02 16:54:44 -0300

    Add exercise 5 in the folder

--- ex5.c
+++ ex5.c
@@ -1,1 +1,16 @@
-/**/
+/*Crie um programa em C que receba um vetor de números reais
+com 100 elementos. Escreva uma função recursiva que inverta
+ordem dos elementos presentes no vetor.*/
+
+#include <stdio.h>
+
+int main()
+{
+
+
+    for (int i = 0; i < 100; i++)
+    {
+        //scanf("%d", &vet[i]);
+    }
+    printf("Digite");
+}
